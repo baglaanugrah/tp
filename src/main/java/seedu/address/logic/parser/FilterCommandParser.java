30b93ab3dddcb054785f803706d29627bf6e0f7c
Manya <e1459283@u.nus.edu>
2026-03-12 21:02:53 +0800

    Fix build issues

--- src/main/java/seedu/address/logic/parser/FilterCommandParser.java
+++ src/main/java/seedu/address/logic/parser/FilterCommandParser.java
@@ -14,7 +14,7 @@ public class FilterCommandParser implements Parser<FilterCommand> {
      * Parses the given {@code String} of arguments in the context of the
      * FilterCommand
      * and returns a FilterCommand object for execution.
-     * 
+     *
      * @throws ParseException if the user input does not conform the expected format
      */
     public FilterCommand parse(String args) throws ParseException {
@@ -26,5 +26,4 @@ public class FilterCommandParser implements Parser<FilterCommand> {
 
         return new FilterCommand();
     }
-
 }
